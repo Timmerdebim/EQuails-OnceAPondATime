8b89c701cf8b264d70c65630c83f2a7caca41047
Timmerdebim <timmerdebim@gmail.com>
2026-01-11 18:56:05 +0100

    Items V.2
    Added all the items & their info (missing their sprites)

--- ForageGame/Assets/Modules/InventorySystem/Item.cs
+++ ForageGame/Assets/Modules/InventorySystem/Item.cs
@@ -12,6 +12,7 @@ public class Item : ScriptableObject
     public string description;
     public Sprite icon;
     public bool isConsumable;
+    public bool returnBottle;
     public float consumableEnergy;
     public GameObject worldPrefab; // The prefab to spawn when dropped
 }
