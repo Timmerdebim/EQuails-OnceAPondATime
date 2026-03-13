3ffe075d159c1b88f554150ec01db55c754c47b1
Timmerdebim <timmerdebim@gmail.com>
2026-03-13 12:14:22 +0100

    Fix

--- ForageGame/Assets/Modules/Features/Gadgets/Switch/SwitchAnimator.cs
+++ ForageGame/Assets/Modules/Features/Gadgets/Switch/SwitchAnimator.cs
@@ -1,75 +1,75 @@
-using System;
-using DG.Tweening;
-using UnityEditor.UIElements;
-using UnityEngine;
-using UnityEngine.Events;
+// using System;
+// using DG.Tweening;
+// using UnityEditor.UIElements;
+// using UnityEngine;
+// using UnityEngine.Events;
 
-namespace TDK.Gadgets
-{
-    public class HindgeAnimator : MonoBehaviour
-    {
-        [SerializeField] private Transform _hingeTransform;
-        [SerializeField] private Vector3 _openAngle = new(30, 0, 0);
-        [SerializeField] private float _openDuration = 1;
-        [SerializeField] private CurveField _openCurve;
-        [SerializeField] private Vector3 _closedAngle = new(30, 0, 0);
-        [SerializeField] private float _closeDuration = 1;
-        [SerializeField] private CurveField _closeCurve;
-        private Sequence sequence;
+// namespace TDK.Gadgets
+// {
+//     public class HindgeAnimator : MonoBehaviour
+//     {
+//         [SerializeField] private Transform _hingeTransform;
+//         [SerializeField] private Vector3 _openAngle = new(30, 0, 0);
+//         [SerializeField] private float _openDuration = 1;
+//         [SerializeField] private CurveField _openCurve;
+//         [SerializeField] private Vector3 _closedAngle = new(30, 0, 0);
+//         [SerializeField] private float _closeDuration = 1;
+//         [SerializeField] private CurveField _closeCurve;
+//         private Sequence sequence;
 
-        public void ToStateOn(Action callback = null, bool instant = false)
-        {
-            if (instant) ToStateOnInstant(callback);
-            else ToStateOnAnimated(callback);
-        }
+//         public void ToStateOn(Action callback = null, bool instant = false)
+//         {
+//             if (instant) ToStateOnInstant(callback);
+//             else ToStateOnAnimated(callback);
+//         }
 
-        public void ToStateOnInstant(Action callback = null)
-        {
-            sequence?.Kill();
-            _hingeTransform.localEulerAngles = _rotationAngle * Vector3.forward;
-            callback?.Invoke();
-        }
+//         public void ToStateOnInstant(Action callback = null)
+//         {
+//             sequence?.Kill();
+//             _hingeTransform.localEulerAngles = _rotationAngle * Vector3.forward;
+//             callback?.Invoke();
+//         }
 
-        public void ToStateOnAnimated(Action callback = null)
-        {
-            sequence?.Kill();
-            sequence = DOTween.Sequence()
-            .Append(_hingeTransform.DOLocalRotate(_rotationAngle * Vector3.forward, _duration))
-            .OnComplete(() => callback?.Invoke());
-        }
+//         public void ToStateOnAnimated(Action callback = null)
+//         {
+//             sequence?.Kill();
+//             sequence = DOTween.Sequence()
+//             .Append(_hingeTransform.DOLocalRotate(_rotationAngle * Vector3.forward, _duration))
+//             .OnComplete(() => callback?.Invoke());
+//         }
 
-        public void AnimateInstant(float targetAngle)
-        {
-            sequence?.Kill();
-            _hingeTransform.localEulerAngles = targetAngle * Vector3.forward;
-            callback?.Invoke();
-        }
+//         public void AnimateInstant(float targetAngle)
+//         {
+//             sequence?.Kill();
+//             _hingeTransform.localEulerAngles = targetAngle * Vector3.forward;
+//             callback?.Invoke();
+//         }
 
-        public void Animate()
+//         public void Animate() { }
 
-        public void ToStateOff(Action callback = null, bool instant = false)
-        {
-            if (instant) ToStateOffInstant(callback);
-            else ToStateOffAnimated(callback);
-        }
-        public void ToStateOffInstant(Action callback = null)
-        {
-            sequence?.Kill();
-            _hingeTransform.localEulerAngles = -_rotationAngle * Vector3.forward;
-            callback?.Invoke();
-        }
+//         public void ToStateOff(Action callback = null, bool instant = false)
+//         {
+//             if (instant) ToStateOffInstant(callback);
+//             else ToStateOffAnimated(callback);
+//         }
+//         public void ToStateOffInstant(Action callback = null)
+//         {
+//             sequence?.Kill();
+//             _hingeTransform.localEulerAngles = -_rotationAngle * Vector3.forward;
+//             callback?.Invoke();
+//         }
 
-        public void ToStateOffAnimated(Action callback = null)
-        {
-            sequence?.Kill();
-            sequence = DOTween.Sequence()
-            .Append(_hingeTransform.DOLocalRotate(-_rotationAngle * Vector3.forward, _duration))
-            .OnComplete(() => callback?.Invoke());
-        }
+//         public void ToStateOffAnimated(Action callback = null)
+//         {
+//             sequence?.Kill();
+//             sequence = DOTween.Sequence()
+//             .Append(_hingeTransform.DOLocalRotate(-_rotationAngle * Vector3.forward, _duration))
+//             .OnComplete(() => callback?.Invoke());
+//         }
 
-        void OnDestroy()
-        {
-            sequence?.Kill();
-        }
-    }
-}
+//         void OnDestroy()
+//         {
+//             sequence?.Kill();
+//         }
+//     }
+// }
--- ForageGame/Assets/Modules/Features/Gadgets/Switch/SwitchController.cs
+++ ForageGame/Assets/Modules/Features/Gadgets/Switch/SwitchController.cs
@@ -1,35 +1,35 @@
-using Assets.Modules.Interaction;
-using TDK.GadgetSystem.Runtime.Gadgets;
-using UnityEngine;
+// using Assets.Modules.Interaction;
+// using TDK.GadgetSystem.Runtime.Gadgets;
+// using UnityEngine;
 
-namespace TDK.Gadgets
-{
-    [RequireComponent(typeof(SwitchGadget))]
-    [RequireComponent(typeof(Animator))]
-    public class SwitchController : MonoBehaviour, IInteractable
-    {
-        private SwitchGadget switchGadget;
-        private Animator animator;
-        void Awake()
-        {
-            switchGadget = GetComponent<SwitchGadget>();
-            animator = GetComponent<Animator>();
-        }
+// namespace TDK.Gadgets
+// {
+//     [RequireComponent(typeof(SwitchGadget))]
+//     [RequireComponent(typeof(Animator))]
+//     public class SwitchController : MonoBehaviour, IInteractable
+//     {
+//         private SwitchGadget switchGadget;
+//         private Animator animator;
+//         void Awake()
+//         {
+//             switchGadget = GetComponent<SwitchGadget>();
+//             animator = GetComponent<Animator>();
+//         }
 
-        public void Interact()
-        {
-            switchGadget.Toggle();
-            animator.SetBool("isOpen", switchGadget.IsActive)
-        }
+//         public void Interact()
+//         {
+//             switchGadget.Toggle();
+//             animator.SetBool("isOpen", switchGadget.IsActive);
+//         }
 
-        public void Focus()
-        {
-            throw new System.NotImplementedException();
-        }
+//         public void Focus()
+//         {
+//             throw new System.NotImplementedException();
+//         }
 
-        public void Unfocus()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-}
+//         public void Unfocus()
+//         {
+//             throw new System.NotImplementedException();
+//         }
+//     }
+// }
