0c6d7fbbe5723f52f939a0c14b07c0e3609d1aec
HaentjeGH <l.m.j.haenen@student.tue.nl>
2026-04-08 16:22:59 +0200

    Clean up dependencies

--- ForageGame/Assets/Modules/Core/NPCSystem/NpcController.cs
+++ ForageGame/Assets/Modules/Core/NPCSystem/NpcController.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace NPC
 {
+    /// <summary>
+    /// Allows for a serializable dictionary in the inspector
+    /// </summary>
+    [System.Serializable]
+    public struct DialogueActionEntry
+    {
+        public string dialogueAction;
+        public UnityEvent gadgetAction;
+    }
+
     public class NpcController : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [SerializeField] private List<DialogueActionEntry> dialogueActionMap; //serializable dict in inspector...
+        [SerializeField] private Dictionary<string, UnityEvent> dialogueActions; //...actual dict at runtime
+
+        void Awake()
         {
-            
+            dialogueActions = new Dictionary<string, UnityEvent>();
+            foreach (var entry in dialogueActionMap)
+                dialogueActions[entry. dialogueAction] = entry.gadgetAction;
         }
 
         // Update is called once per frame
