e87a572f30870c55da71688231ddff14ebd15529
HaentjeGH <l.m.j.haenen@student.tue.nl>
2026-09-12 13:39:25 +0200

    Not all story flags pass time

--- ForageGame/Assets/Modules/Core/Story Flags/StoryFlagSO.cs
+++ ForageGame/Assets/Modules/Core/Story Flags/StoryFlagSO.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class StoryFlag : ScriptableObject
 {
     // cons
-    public StoryFlag(string id)
+    public StoryFlag(string id, bool passesTime = true)
     {
         this.id = id;
+        this.passesTime = passesTime;
     }
     //must string match with the event flags in Dialogue.txt
     public string id;
+
+    [Tooltip("Certain Flags are used for cutscenes and other multi-npc syncing stuff. So not all story flags should pass time.")]
+    public bool passesTime = true;
 }
