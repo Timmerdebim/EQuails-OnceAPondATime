dcbd55eda525e214a99f9712102d18bd6e127e26
Timmerdebim <timmerdebim@gmail.com>
2026-09-15 15:06:09 +0200

    better popups

--- ForageGame/Assets/Modules/Features/Popup Textbox/PopupTextbox.cs
+++ ForageGame/Assets/Modules/Features/Popup Textbox/PopupTextbox.cs
@@ -5,6 +5,12 @@ public class PopupTextbox : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private TMP_Text _textbox;
+    [SerializeField] private TMProOutline _textOutline;
+
+    void Start()
+    {
+        SetTextColor(_textbox.color);
+    }
 
     public void SetText(string text)
     {
@@ -14,6 +20,11 @@ public class PopupTextbox : MonoBehaviour
     public void SetTextColor(Color color)
     {
         _textbox.color = color;
+        Color.RGBToHSV(color, out float hue, out float saturation, out float value); //HSV are all 0 to 1
+        hue = (float)((hue + 0.5) % 1); // complementary hue to improve contrast
+        value = (float)((value + 0.5) % 1); // complementary value to improve visability
+        saturation /= 2; // desaturate to not stand out over the main text
+        _textOutline.SetColor(Color.HSVToRGB(hue, saturation, value));
     }
 
     public void ShowTextbox(bool showTextbox)
--- ForageGame/Assets/TMProOutline.cs
+++ ForageGame/Assets/TMProOutline.cs
@@ -2,10 +2,10 @@ using System;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(TextMeshProUGUI))]
+[RequireComponent(typeof(TMP_Text))]
 public class TMProOutline : MonoBehaviour
 {
-    TextMeshProUGUI _textView;
+    TMP_Text _text;
     [SerializeField] float outlineWidth = 1.0f;
     [SerializeField] Color outlineColor = Color.white;
     private void Awake()
@@ -13,32 +13,31 @@ public class TMProOutline : MonoBehaviour
         SetOutline();
     }
 
+    public void SetColor(Color color)
+    {
+        outlineColor = color;
+        SetOutline();
+    }
+
     [ContextMenu("SetOutline")]
     private void SetOutline()
     {
-        if(_textView == null) _textView = GetComponent<TextMeshProUGUI>();
+        if (_text == null) _text = GetComponent<TMP_Text>();
 
         // 1. Get a copy of the material (Creates a new instance for this object)
         // If you use 'fontSharedMaterial', you will change it for EVERY text using this font!
-        Material mat = _textView.fontMaterial;
+        Material mat = _text.fontMaterial;
 
         // 2. Enable the Outline keyword (Required for some shaders to switch modes)
         mat.EnableKeyword("OUTLINE_ON");
 
         // 3. Set the Shader properties
         // Width is usually between 0.0 and 1.0
-        mat.SetFloat("_OutlineWidth", outlineWidth); 
+        mat.SetFloat("_OutlineWidth", outlineWidth);
         mat.SetColor("_OutlineColor", outlineColor);
 
         // 4. IMPORTANT: Force TMP to update its mesh boundaries
         // Without this, the outline might get "clipped" or cut off at the edges
-        _textView.UpdateMeshPadding();
+        _text.UpdateMeshPadding();
     }
-
-#if  UNITY_EDITOR
-    private void OnValidate()
-    {
-        SetOutline();
-    }
-#endif
 }
