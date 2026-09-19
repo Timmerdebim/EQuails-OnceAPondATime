94741d111fcceda338d07114f04486886454be1e
Timmerdebim <timmerdebim@gmail.com>
2026-09-19 21:13:18 +0200

    grafic

--- ForageGame/Assets/Modules/Core/Menu/Settings/Graphics/GraphicsSettingsManager.cs
+++ ForageGame/Assets/Modules/Core/Menu/Settings/Graphics/GraphicsSettingsManager.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
-using System.IO;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Project.Menus.Graphics
 {
     public class GraphicsSettingsManager : MonoBehaviour
     {
+        [SerializeField] private RenderPipelineAsset[] _urpAssets; // low to high
+
         public static GraphicsSettingsManager Instance { get; private set; }
 
         private void Awake()
@@ -17,46 +21,42 @@ namespace Project.Menus.Graphics
             Instance = this;
 
             // Load & Apply All Settings
+            FullScreen = FullScreen;
             Resolution = Resolution;
-            Quality = Quality;
-            Vsync = Vsync;
             Framerate = Framerate;
+            TextureQuality = TextureQuality;
+            LightingQuality = LightingQuality;
+            TerrainQuality = TerrainQuality;
+            VSync = VSync;
+            AnisotropicTextures = AnisotropicTextures;
         }
 
         // ------------ Settings ------------
 
+        public int FullScreen
+        {
+            // 0 = off, 1 = on.
+            get => PlayerPrefs.GetInt("FullScreen", 1);
+            set
+            {
+                Screen.fullScreen = (value == 1);
+                PlayerPrefs.SetInt("FullScreen", value);
+                PlayerPrefs.Save();
+            }
+        }
+
         public int Resolution
         {
-            get => PlayerPrefs.GetInt("Resolution", 0); // TODO
+            get => PlayerPrefs.GetInt("Resolution",
+                // Default is the actual screen resolution (if possible), else its 1080p.
+                GetDefaultResolutionIndex()
+                );
             set
             {
                 if (-1 < value && value < Screen.resolutions.Length)
                     Screen.SetResolution(Screen.resolutions[value].width, Screen.resolutions[value].height, Screen.fullScreen);
 
                 PlayerPrefs.SetInt("Resolution", value);
-                PlayerPrefs.Save();
-                Screen.SetResolution(1920, 1080, true);
-            }
-        }
-
-        public int Quality
-        {
-            get => PlayerPrefs.GetInt("Quality", 0); // TODO
-            set
-            {
-                QualitySettings.SetQualityLevel(value);
-                PlayerPrefs.SetInt("Quality", value);
-                PlayerPrefs.Save();
-            }
-        }
-
-        public int Vsync
-        {
-            get => PlayerPrefs.GetInt("Vsync", 0);
-            set
-            {
-                QualitySettings.vSyncCount = value;
-                PlayerPrefs.SetInt("Vsync", value);
                 PlayerPrefs.Save();
             }
         }
@@ -70,6 +70,103 @@ namespace Project.Menus.Graphics
                 PlayerPrefs.SetInt("Framerate", value);
                 PlayerPrefs.Save();
             }
+        }
+
+        public int TextureQuality
+        {
+            // 0 = full resolution, 1 = half, 2 = quarter, 3 = eighths. [FLIP THE LIST]
+            // 0 = Terrible, 1 = Low, 2 = Medium, 3 = High.
+            get => PlayerPrefs.GetInt("TextureQuality", 3);
+            set
+            {
+                QualitySettings.globalTextureMipmapLimit = 3 - value;
+                PlayerPrefs.SetInt("TextureQuality", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public int LightingQuality
+        {
+
+            // 0 = Terrible, 1 = Low, 2 = Medium, 3 = High, 4 = Ridiculous.
+            get => PlayerPrefs.GetInt("TextureQuality", 3);
+            set
+            {
+                if (_urpAssets[value] != null)
+                    QualitySettings.renderPipeline = _urpAssets[value];
+                PlayerPrefs.SetInt("TextureQuality", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public int TerrainQuality
+        {
+            // 0 = Terrible, 1 = Low, 2 = Medium, 3 = High.
+            get => PlayerPrefs.GetInt("TerrainQuality", 3);
+            set
+            {
+                if (value == 0) QualitySettings.terrainPixelError = 20;
+                else if (value == 1) QualitySettings.terrainPixelError = 10;
+                else if (value == 2) QualitySettings.terrainPixelError = 5;
+                else if (value == 3) QualitySettings.terrainPixelError = 1;
+                PlayerPrefs.SetInt("TerrainQuality", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public int VSync
+        {
+            // 0 = off, 1 = on.
+            get => PlayerPrefs.GetInt("VSync", 0);
+            set
+            {
+                QualitySettings.vSyncCount = value;
+                PlayerPrefs.SetInt("VSync", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public int AnisotropicTextures
+        {
+            // 0 = Off, 1 = On.
+            get => PlayerPrefs.GetInt("AnisotropicTextures", 1);
+            set
+            {
+                if (value == 0) QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+                else QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+                PlayerPrefs.SetInt("AnisotropicTextures", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        // ------- HELPERS ------
+
+        private static int GetDefaultResolutionIndex()
+        {
+            Resolution current = Screen.currentResolution;
+
+            // Prefer the current screen resolution.
+            for (int i = 0; i < Screen.resolutions.Length; i++)
+            {
+                if (Screen.resolutions[i].width == current.width &&
+                    Screen.resolutions[i].height == current.height)
+                {
+                    return i;
+                }
+            }
+
+            // Otherwise fall back to 1920x1080.
+            for (int i = 0; i < Screen.resolutions.Length; i++)
+            {
+                if (Screen.resolutions[i].width == 1920 &&
+                    Screen.resolutions[i].height == 1080)
+                {
+                    return i;
+                }
+            }
+
+            // Last resort: first available resolution.
+            return Screen.resolutions.Length > 0 ? 0 : -1;
         }
     }
 }
--- ForageGame/Assets/Modules/Core/Menu/Settings/Graphics/GraphicsSettingsMenu.cs
+++ ForageGame/Assets/Modules/Core/Menu/Settings/Graphics/GraphicsSettingsMenu.cs
@@ -11,10 +11,14 @@ namespace Project.Menus.Graphics
     public class GraphicsSettingsMenu : Menu
     {
         [Header("UI References")]
+        [SerializeField] private SelectionUIElement _fullscreen;
         [SerializeField] private SelectionUIElement _resolution;
-        [SerializeField] private SelectionUIElement _quality;
-        [SerializeField] private SelectionUIElement _vsync;
         [SerializeField] private Slider _framerate;
+        [SerializeField] private SelectionUIElement _textureQuality;
+        [SerializeField] private SelectionUIElement _lightingQuality;
+        [SerializeField] private SelectionUIElement _terrainQuality;
+        [SerializeField] private SelectionUIElement _vSync;
+        [SerializeField] private SelectionUIElement _anisotropicTextures;
 
         public override void OnEnteringMenu()
         {
@@ -22,45 +26,44 @@ namespace Project.Menus.Graphics
             List<string> resolutionOptions = new();
             for (int i = 0; i < Screen.resolutions.Length; i++)
             {
-                string resolutionOption = $"{Screen.resolutions[i].width} x {Screen.resolutions[i].height}";
+                string resolutionOption = $"{Screen.resolutions[i].width}x{Screen.resolutions[i].height}";
                 resolutionOptions.Add(resolutionOption);
             }
             _resolution.SetOptions(resolutionOptions.ToArray());
 
-            // Quality
-            _quality.SetOptions(QualitySettings.names);
+            _fullscreen.SetOptions(new string[] { "Off", "On" });
+            _textureQuality.SetOptions(new string[] { "Terrible", "Low", "Medium", "High" });
+            _lightingQuality.SetOptions(new string[] { "Unplayable", "Low", "Medium", "High", "Ridiculous" });
+            _terrainQuality.SetOptions(new string[] { "Too Low", "Low", "Medium", "High" });
+            _vSync.SetOptions(new string[] { "Off", "On" });
+            _anisotropicTextures.SetOptions(new string[] { "Off", "On" });
 
-            // Vsync
-            _vsync.SetOptions(new string[] { "Off", "On" });
-
-            _resolution.SetCurrentOption(GraphicsSettingsManager.Instance.Resolution);
-            _quality.SetCurrentOption(GraphicsSettingsManager.Instance.Quality);
-            _vsync.SetCurrentOption(GraphicsSettingsManager.Instance.Vsync);
-            _framerate.value = GraphicsSettingsManager.Instance.Framerate;
+            RefreshVisuals();
         }
 
         // ------------ Buttons ------------
 
-        public void OnResolutionChanged()
-        {
-            GraphicsSettingsManager.Instance.Resolution = _resolution._currentOption;
-        }
-
-        public void OnQualityChanged()
-        {
-            GraphicsSettingsManager.Instance.Quality = _quality._currentOption;
-        }
-
-        public void OnVsyncChanged()
-        {
-            GraphicsSettingsManager.Instance.Vsync = _vsync._currentOption;
-        }
-
-        public void OnFramerateChanged()
-        {
-            GraphicsSettingsManager.Instance.Framerate = Mathf.RoundToInt(_framerate.value);
-        }
+        public void OnFullscreenChanged() => GraphicsSettingsManager.Instance.FullScreen = _fullscreen._currentOption;
+        public void OnResolutionChanged() => GraphicsSettingsManager.Instance.Resolution = _resolution._currentOption;
+        public void OnFramerateChanged() => GraphicsSettingsManager.Instance.Framerate = Mathf.RoundToInt(_framerate.value);
+        public void OnTextureQualityChanged() => GraphicsSettingsManager.Instance.TextureQuality = _textureQuality._currentOption;
+        public void OnLightingQualityChanged() => GraphicsSettingsManager.Instance.LightingQuality = _lightingQuality._currentOption;
+        public void OnTerrainQualityChanged() => GraphicsSettingsManager.Instance.TerrainQuality = _terrainQuality._currentOption;
+        public void OnVSyncChanged() => GraphicsSettingsManager.Instance.VSync = _vSync._currentOption;
+        public void OnAnisotropicTexturesChanged() => GraphicsSettingsManager.Instance.AnisotropicTextures = _anisotropicTextures._currentOption;
 
         // ------------ Functions ------------
+
+        public void RefreshVisuals()
+        {
+            _fullscreen.SetCurrentOption(GraphicsSettingsManager.Instance.FullScreen);
+            _resolution.SetCurrentOption(GraphicsSettingsManager.Instance.Resolution);
+            _framerate.value = GraphicsSettingsManager.Instance.Framerate;
+            _textureQuality.SetCurrentOption(GraphicsSettingsManager.Instance.TextureQuality);
+            _lightingQuality.SetCurrentOption(GraphicsSettingsManager.Instance.LightingQuality);
+            _terrainQuality.SetCurrentOption(GraphicsSettingsManager.Instance.TerrainQuality);
+            _vSync.SetCurrentOption(GraphicsSettingsManager.Instance.VSync);
+            _anisotropicTextures.SetCurrentOption(GraphicsSettingsManager.Instance.AnisotropicTextures);
+        }
     }
 }
