1f1595f93f7d78ab631730107f6e226aa6a39363
Timmerdebim <timmerdebim@gmail.com>
2026-09-24 15:27:49 +0200

    textbox color fix

--- ForageGame/Assets/Modules/Features/Popup Textbox/PopupTextbox.cs
+++ ForageGame/Assets/Modules/Features/Popup Textbox/PopupTextbox.cs
@@ -7,10 +7,11 @@ public class PopupTextbox : MonoBehaviour
     [SerializeField] private TMP_Text _textbox;
     [SerializeField] private TMProOutline _textOutline;
     private string _text = "";
+    private Color _color = Color.black;
 
     void Start()
     {
-        SetTextColor(_textbox.color);
+        _color = _textbox.color;
         _text = _textbox.text;
     }
 
@@ -21,17 +22,23 @@ public class PopupTextbox : MonoBehaviour
 
     public void SetTextColor(Color color)
     {
-        _textbox.color = color;
-        Color.RGBToHSV(color, out float hue, out float saturation, out float value); //HSV are all 0 to 1
-        hue = (float)((hue + 0.5) % 1); // complementary hue to improve contrast
-        value = (float)((value + 0.5) % 1); // complementary value to improve visability
-        saturation /= 2; // desaturate to not stand out over the main text
-        _textOutline.SetColor(Color.HSVToRGB(hue, saturation, value));
+        _color = color;
     }
 
     public void ShowTextbox(bool showTextbox)
     {
-        if (showTextbox) _textbox.text = _text; // update text right before revealing
+        if (showTextbox)
+        {
+            _textbox.text = _text; // update text right before revealing
+
+            _textbox.color = _color;
+            Color.RGBToHSV(_color, out float hue, out float saturation, out float value); //HSV are all 0 to 1
+            hue = (float)((hue + 0.5) % 1); // complementary hue to improve contrast
+            value = (float)((value + 0.5) % 1); // complementary value to improve visability
+            saturation /= 2; // desaturate to not stand out over the main text
+            _textOutline.SetColor(Color.HSVToRGB(hue, saturation, value));
+        }
+
         _animator.SetBool("Show", showTextbox);
     }
 }
